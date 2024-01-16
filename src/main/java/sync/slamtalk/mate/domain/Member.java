b0584c4dfbdc1c69492f598ce868df9dcb9b11b1
Dong Soo <rsy48@naver.com>
2024-01-16 22:55:37 +0900

    같이하기 테이블 및 매핑 테이블 생성

---
+++ src/main/java/sync/slamtalk/mate/domain/Member.java
@@ -0,0 +1,32 @@
+package sync.slamtalk.mate.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Getter
+public class Member {   // 다른 엔터티의 생성을 위한 임시적인 목업 엔터티
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private long id;
+
+
+    private String name;
+
+    @OneToMany(mappedBy = "member")
+    private List<Participant> participants;
+
+    public Member() {
+    }
+
+    public Member(String name) {
+        this.name = name;
+    }
+}
