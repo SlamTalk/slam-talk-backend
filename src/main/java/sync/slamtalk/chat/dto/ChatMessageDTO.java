227212baa2b701269057180da06dabd9ab4c5f59
yj120 <yeji3852@daum.net>
2024-01-17 22:53:07 +0900

    refactor : 채팅메세지DTO 수정,메세지매핑컨트롤러 수정

--- src/main/java/sync/slamtalk/chat/dto/ChatMessageDTO.java
+++ src/main/java/sync/slamtalk/chat/dto/ChatMessageDTO.java
@@ -1,5 +1,7 @@
 package sync.slamtalk.chat.dto;
 
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.io.Serializable;
@@ -9,12 +11,19 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor
 @Builder(access = AccessLevel.PUBLIC)
 public class ChatMessageDTO implements Serializable {
+    @NotNull
     private String roomId; // 채팅방 아이디(채팅방 식별자)
     private String senderId; // 메세지를 보낸 사용자의 고유 식별자
+    @Nullable
     private String content; // 메세지 내용
     private LocalDateTime timestamp; // 메세지를 보낸 시간
     private Boolean read; // 읽음 여부를 나타내는 상태 필드
+
+
+    // 생성될 때 현재 시간을 저장
+    public ChatMessageDTO(){
+        this.timestamp = LocalDateTime.now();
+    }
 }
