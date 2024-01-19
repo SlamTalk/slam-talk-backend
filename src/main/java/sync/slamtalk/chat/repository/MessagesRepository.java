7e0e3f55638c94a45b078d1f97dd6d58825f03cd
yj120 <yeji3852@daum.net>
2024-01-19 15:23:46 +0900

    refactor : Import 문 최적화

--- src/main/java/sync/slamtalk/chat/repository/MessagesRepository.java
+++ src/main/java/sync/slamtalk/chat/repository/MessagesRepository.java
@@ -1,7 +1,6 @@
 package sync.slamtalk.chat.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import sync.slamtalk.chat.entity.ChatRoom;
 import sync.slamtalk.chat.entity.Messages;
 
 public interface MessagesRepository extends JpaRepository<Messages,Long> {
