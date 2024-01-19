1b2631b68e8d9ef023608c2b4e9376efe6750091
yj120 <yeji3852@daum.net>
2024-01-19 15:21:05 +0900

    feat : 채팅엔터티,레포지토리 생성, dto 수정

---
+++ src/main/java/sync/slamtalk/chat/repository/ChatRoomRepository.java
@@ -0,0 +1,15 @@
+package sync.slamtalk.chat.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import sync.slamtalk.chat.entity.ChatRoom;
+import sync.slamtalk.chat.entity.UserChatRoom;
+import sync.slamtalk.chat.entity.UserMock;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+
+}
---
+++ src/main/java/sync/slamtalk/chat/repository/MessagesRepository.java
@@ -0,0 +1,8 @@
+package sync.slamtalk.chat.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sync.slamtalk.chat.entity.ChatRoom;
+import sync.slamtalk.chat.entity.Messages;
+
+public interface MessagesRepository extends JpaRepository<Messages,Long> {
+}
