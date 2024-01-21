e69f7da8411b61d7c92df25f9630d074652f7ea2
yj120 <yeji3852@daum.net>
2024-01-21 19:03:04 +0900

    refactor : entity 필드 추가

--- src/main/java/sync/slamtalk/chat/entity/RoomType.java
+++ src/main/java/sync/slamtalk/chat/entity/RoomType.java
@@ -7,8 +7,12 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum RoomType {
-    // 일대일, 단체
-    DIRECT("DM"), TOGETHER("TM");
+    /* 다이렉트 채팅, 농구장 채팅, 같이해요 채팅, 팀 매칭 채팅 */
+
+    DIRECT("DM"), // 다이렉트 메세지
+    TOGETHER("TM"), // 같이 해요 메세지
+    BASKETBALL("BM"), // 농구장 메세지
+    MATCHING("MM"); // 매칭 메세지
 
     private final String key;
 }
