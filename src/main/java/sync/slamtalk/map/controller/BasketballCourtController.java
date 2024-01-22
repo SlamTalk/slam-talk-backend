1c607a6355dc9d916b6a0b65dbefcc7e4b8d7f05
sh-9610 <fkfkthth@naver.com>
2024-01-22 14:59:07 +0900

    refactor : swagger 적용 및 반환 값 responseentity -> apiresponse 변경

--- src/main/java/sync/slamtalk/map/controller/BasketballCourtController.java
+++ src/main/java/sync/slamtalk/map/controller/BasketballCourtController.java
@@ -1,9 +1,9 @@
 package sync.slamtalk.map.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,18 +21,28 @@ public class BasketballCourtController {
 
     //전체 농구장 간략 정보
     @GetMapping
-    public ResponseEntity<ApiResponse<List<BasketballCourtDto>>> getAllCourtSummaryInfo() {
+    @Operation(
+            summary = "전체 농구장 간략 정보", // 기능 제목 입니다
+            description = "이 기능은 마커에 띄울 전체 농구장의 간략 정보 응답을 보내는 기능입니다.", // 기능 설명
+            tags = {"지도"}
+    )
+    public ApiResponse<List<BasketballCourtDto>> getAllCourtSummaryInfo() {
         List<BasketballCourtDto> courtDetails = basketballCourtService.getAllCourtSummaryInfo();
-        return ResponseEntity.ok(ApiResponse.ok(courtDetails, "농구장 목록을 성공적으로 가져왔습니다."));
+        return (ApiResponse.ok(courtDetails, "농구장 목록을 성공적으로 가져왔습니다."));
     }
 
 
     //특정 농구장 전체 정보
     @GetMapping("/{courtId}")
-    public ResponseEntity<ApiResponse<BasketballCourtDto>> getCourtFullInfoById(@PathVariable Long courtId) {
+    @Operation(
+            summary = "마커 클릭 농구장 전체 정보", // 기능 제목 입니다
+            description = "이 기능은 클릭한 마커에 해당하는 농구장의 전체 정보 응답을 보내는 기능입니다.", // 기능 설명
+            tags = {"지도"}
+    )
+    public ApiResponse<BasketballCourtDto> getCourtFullInfoById(@PathVariable Long courtId) {
         Optional<BasketballCourtDto> basketballCourtDto = basketballCourtService.getCourtFullInfoById(courtId);
         return basketballCourtDto
-                .map(dto -> ResponseEntity.ok(ApiResponse.ok(dto, "농구장 상세 정보를 성공적으로 가져왔습니다.")))
-                .orElseGet(() -> ResponseEntity.ok(ApiResponse.fail("농구장 정보를 찾을 수 없습니다.")));
+                .map(dto -> (ApiResponse.ok(dto, "농구장 상세 정보를 성공적으로 가져왔습니다.")))
+                .orElseGet(() -> (ApiResponse.fail("농구장 정보를 찾을 수 없습니다.")));
     }
 }
