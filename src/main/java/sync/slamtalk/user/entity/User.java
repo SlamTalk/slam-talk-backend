e6df32137b11a388210ca6941d4ec360e2c2d402
leebongseung <yui5227@gmail.com>
2024-01-23 16:46:53 +0900

    feat: user관련 entity, dto, repo 수정

--- src/main/java/sync/slamtalk/user/entity/User.java
+++ src/main/java/sync/slamtalk/user/entity/User.java
@@ -1,19 +1,24 @@
 package sync.slamtalk.user.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import sync.slamtalk.common.BaseEntity;
 import sync.slamtalk.user.dto.UserSignUpRequestDto;
 
+import java.util.Collection;
+import java.util.Collections;
+
 @Entity
 @Table(name = "users") // User가 예약어라서 users로 테이블이름 명시
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
+@EqualsAndHashCode(of = "id", callSuper = false)
 @Getter
-public class User extends BaseEntity {
+public class User extends BaseEntity implements UserDetails {
     @Id  @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false)
     private Long id;
@@ -50,13 +55,26 @@ public class User extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private UserBasketballPositionType basketballPosition;
 
-    /* 알람 기능*/
+    /* 알람 기능 */
     @Column(name = "is_alarm_set")
     private String isAlarmSet;
 
-    /* 레벨 시스템 기능*/
+    /* 레벨 시스템 기능 */
     @Column(name = "level_score", nullable = false)
     private Long levelScore = 0L;
+
+    /* 연관 관계 매핑 */
+/*
+    // todo : 예지님 연관관계 매핑 부분
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<UserChatRoom> userChatRooms = new ArrayList<>();
+*/
+
+/*
+    //todo : 동수님 연관관계 매핑 부분
+    @OneToMany(mappedBy = "writerId", fetch = FetchType.LAZY)
+    private List<MatePost> matePosts = new ArrayList<>();
+*/
 
     /**
      * 비밀번호 암호화 메소드
@@ -68,8 +86,8 @@ public class User extends BaseEntity {
 
     /**
      * userSignUpDto 를 User로 변환
-     * @param userSignUpDto
-     * @return user
+     * @param userSignUpDto 유저회원가입 dto
+     * @return user 유저 entity
      * */
     public static User from(UserSignUpRequestDto userSignUpDto) {
 
@@ -81,5 +99,41 @@ public class User extends BaseEntity {
         user.levelScore = 0L;
         user.socialType = SocialType.LOCAL;
         return user;
+    }
+
+    public void updateRefreshToken(String refreshToken){
+        this.refreshToken = refreshToken;
+    }
+
+
+    /* UserDetails 관련 메서드 */
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority(this.getRole().getKey()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.id.toString();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
