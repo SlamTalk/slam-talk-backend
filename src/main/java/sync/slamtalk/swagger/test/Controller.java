2037876a8b0412f25d335d8030917994138ef203
leebongseung <yui5227@gmail.com>
2024-01-23 16:45:59 +0900

    refactor: swagger test controller 파일 이동

--- src/main/java/sync/slamtalk/swagger/test/Controller.java
+++ src/main/java/sync/slamtalk/swagger/test/Controller.java
@@ -1,15 +1,19 @@
-package sync.slamtalk.test;
+package sync.slamtalk.swagger.test;
 
 import io.swagger.v3.oas.annotations.Operation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sync.slamtalk.common.ApiResponse;
+import sync.slamtalk.user.entity.User;
 
 /**
 *   Swagger 사용 방법 및 테스트 기능
 * */
 
+@Slf4j
 @RestController
 @RequestMapping("/api")
 public class Controller {
@@ -20,7 +24,11 @@ public class Controller {
             tags = {"유저", "관리자"} // 유저인지 관리자기능인지 여부
     )
     // 주의 사항 ApiResponse Swagger 도 동일한 클래스가 존재하기 때문에 import 주의 해야합니다.
-    public ApiResponse hello(String param){
+    public ApiResponse hello(
+            String param,
+            @AuthenticationPrincipal User user
+    ){
+        log.debug("user = {}", user.toString());
         return ApiResponse.ok(param + "성공쓰");
     }
 }
