7b9e0d9efd23a30127fc7a3b988a3b8f2a3e8e60
yj120 <yeji3852@daum.net>
2024-01-25 05:27:25 +0900

    create : test코드 생성

--- src/test/java/sync/slamtalk/chat/repository/ChatRoomRepositoryTest.java
+++ src/test/java/sync/slamtalk/chat/repository/ChatRoomRepositoryTest.java
@@ -1,4 +1,27 @@
+package sync.slamtalk.chat.repository;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import sync.slamtalk.chat.entity.ChatRoom;
+import sync.slamtalk.chat.entity.RoomType;
+import sync.slamtalk.chat.entity.UserChatRoom;
+import sync.slamtalk.chat.entity.UserMock;
+
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
 class ChatRoomRepositoryTest {
-  
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private ChatRoomRepository chatRoomRepository;
+
 }
