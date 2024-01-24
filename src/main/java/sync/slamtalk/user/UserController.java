9cd7963541cd672db29aa1e902a0f026529dd629
leebongseung <yui5227@gmail.com>
2024-01-24 13:06:51 +0900

    feat : 엑세스 토큰 헤더에 저장/ 리프래쉬 토큰 재발급 api 개발

--- src/main/java/sync/slamtalk/user/UserController.java
+++ src/main/java/sync/slamtalk/user/UserController.java
@@ -1,15 +1,14 @@
-package sync.slamtalk.user.controller;
+package sync.slamtalk.user;
 
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sync.slamtalk.common.ApiResponse;
 import sync.slamtalk.security.dto.JwtTokenResponseDto;
 import sync.slamtalk.security.utils.CookieUtil;
@@ -35,39 +34,28 @@ public class UserController {
     private String domain;
     private final UserService userService;
 
+    /**
+     * 로그인 api
+     * @param  userLoginDto
+     * @param response
+     *
+     * @return  jwtTokenResponseDto
+     * */
     @PostMapping("/login")
     @Operation(
             summary = "자체 로그인 기능",
             description = "자체 로그인 기능입니다.",
             tags = {"로그인/회원가입"}
     )
-    public ApiResponse<String> authorize(
+    public ApiResponse<JwtTokenResponseDto> authorize(
             @Valid @RequestBody UserLoginRequestDto userLoginDto,
             HttpServletResponse response
     ) {
         // 1. username + password 를 기반으로 Authentication 객체 생성
         // 이때 authentication 은 인증 여부를 확인하는 authenticated 값이 false
-        log.debug("userLoginDto ={}", userLoginDto);
+        JwtTokenResponseDto jwtTokenResponseDto = userService.login(userLoginDto, response);
 
-        JwtTokenResponseDto jwtTokenResponseDto = userService.login(userLoginDto);
-
-        CookieUtil.addCookie(
-                response,
-                authorizationHeader,
-                jwtTokenResponseDto.getAccessToken(),
-                accessTokenExpirationPeriod,
-                domain
-        );
-
-        CookieUtil.addCookie(
-                response,
-                refreshAuthorizationHeader,
-                jwtTokenResponseDto.getRefreshToken(),
-                refreshTokenExpirationPeriod,
-                domain
-        );
-
-        return ApiResponse.ok("로그인 성공");
+        return ApiResponse.ok(jwtTokenResponseDto);
     }
 
     /**
@@ -85,5 +73,27 @@ public class UserController {
     public ApiResponse<String> signUp(@Valid @RequestBody UserSignUpRequestDto userSignUpDto) {
         userService.signUp(userSignUpDto);
         return ApiResponse.ok("회원가입 성공");
+    }
+
+    /**
+     * 리프래쉬 토큰 재발급 api
+     * @param request
+     * @param response
+     *
+     * @return  JwtTokenResponseDto
+     * */
+    @PatchMapping("/tokens/refresh")
+    @Operation(
+            summary = "엑세스 및 리프레쉬 토큰 재발급",
+            description = "리프레쉬 토큰은 httpOnly secure 쿠키로 보내주고 엑세스 토큰은 헤더와 파라미터에 넣어 보내줍니다.",
+            tags = {"로그인/회원가입"}
+    )
+    public ApiResponse<JwtTokenResponseDto> refreshToken(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ){
+        JwtTokenResponseDto jwtTokenResponseDto = userService.refreshToken(request, response);
+
+        return ApiResponse.ok(jwtTokenResponseDto);
     }
 }
--- src/main/java/sync/slamtalk/user/UserService.java
+++ src/main/java/sync/slamtalk/user/UserService.java
@@ -1,7 +1,11 @@
 package sync.slamtalk.user;
 
+import com.fasterxml.jackson.databind.util.JSONWrappedObject;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
@@ -12,11 +16,15 @@ import org.springframework.transaction.annotation.Transactional;
 import sync.slamtalk.common.BaseException;
 import sync.slamtalk.security.dto.JwtTokenResponseDto;
 import sync.slamtalk.security.jwt.JwtTokenProvider;
+import sync.slamtalk.security.utils.CookieUtil;
 import sync.slamtalk.user.dto.UserLoginRequestDto;
 import sync.slamtalk.user.dto.UserSignUpRequestDto;
 import sync.slamtalk.user.entity.SocialType;
 import sync.slamtalk.user.entity.User;
 import sync.slamtalk.user.error.UserErrorResponseCode;
+
+import java.security.DrbgParameters;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -28,15 +36,28 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private final JwtTokenProvider tokenProvider;
+    @Value("${jwt.access.header}")
+    public String accessAuthorizationHeader;
+    @Value("${jwt.refresh.header}")
+    public String refreshAuthorizationCookieName;
+    @Value("${jwt.refresh.expiration}")
+    private int refreshTokenExpirationPeriod;
+    @Value("${jwt.domain}")
+    private String domain;
 
 
     /**
      * 로그인 시 검증 및 액세스 토큰 리프래쉬 토큰 발급 로직
-     * @param  userLoginDto
+     *
+     * @param userLoginDto
+     * @param response
      * @return JwtTokenDto
-     * */
+     */
     @Transactional
-    public JwtTokenResponseDto login(UserLoginRequestDto userLoginDto) {
+    public JwtTokenResponseDto login(
+            UserLoginRequestDto userLoginDto,
+            HttpServletResponse response
+    ) {
         try {
             // 1. email + password 를 기반으로 Authentication 객체 생성
             UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userLoginDto.getEmail(), userLoginDto.getPassword());
@@ -47,7 +68,12 @@ public class UserService {
             SecurityContextHolder.getContext().setAuthentication(authentication);
 
             // 3. 인증 정보를 기반으로 JWT 토큰 생성)
-            return tokenProvider.createToken((User) authentication.getPrincipal());
+            JwtTokenResponseDto jwtTokenResponseDto = tokenProvider.createToken((User) authentication.getPrincipal());
+
+            response.addHeader(accessAuthorizationHeader, jwtTokenResponseDto.getAccessToken());
+            setRefreshTokenCookie(response, jwtTokenResponseDto);
+
+            return jwtTokenResponseDto;
         } catch (Exception e) {
             throw new BaseException(UserErrorResponseCode.BAD_CREDENTIALS);
         }
@@ -56,8 +82,8 @@ public class UserService {
     /**
      * 회원가입 검증 및 회원가입 로직
      *
-     * @param  userSignUpDto
-     * */
+     * @param userSignUpDto
+     */
     @Transactional
     public void signUp(UserSignUpRequestDto userSignUpDto) {
 
@@ -75,5 +101,54 @@ public class UserService {
         user.passwordEncode(passwordEncoder);
 
         userRepository.save(user);
+    }
+
+    /**
+     * 쿠키에서 리프래쉬 토큰을 추출하여 엑세스 토큰과 리프래쉬 토큰을 재발급하는 메서드
+     *
+     * @param request
+     * @param response
+     * @return JwtTokenResponseDto
+     */
+    @Transactional
+    public JwtTokenResponseDto refreshToken(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) {
+
+        String refreshToken = tokenProvider.getRefreshTokenFromCookie(request);
+
+        Optional<JwtTokenResponseDto> optionalJwtTokenResponseDto = tokenProvider.generateNewAccessToken(refreshToken);
+
+        if (optionalJwtTokenResponseDto.isEmpty()) {
+            throw new BaseException(UserErrorResponseCode.INVALID_TOKEN);
+        }
+
+        JwtTokenResponseDto jwtTokenResponseDto = optionalJwtTokenResponseDto.get();
+
+        /* 엑세스 토큰및 리프레쉬 토큰 저장하는 로직 */
+        response.addHeader(accessAuthorizationHeader, jwtTokenResponseDto.getAccessToken());
+        setRefreshTokenCookie(response, jwtTokenResponseDto);
+        return jwtTokenResponseDto;
+    }
+
+    /**
+     * 쿠키에 리프레쉬 토큰을 저장하는 메서드
+     * @param response
+     * @param jwtTokenResponseDto
+     * */
+    private void setRefreshTokenCookie(
+            HttpServletResponse response,
+            JwtTokenResponseDto jwtTokenResponseDto
+    ) {
+        CookieUtil.addCookie(
+                response,
+                refreshAuthorizationCookieName,
+                jwtTokenResponseDto.getRefreshToken(),
+                refreshTokenExpirationPeriod,
+                domain
+        );
+
+        jwtTokenResponseDto.clearRefreshToken();
     }
 }
