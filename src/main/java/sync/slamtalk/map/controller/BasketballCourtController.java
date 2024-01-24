0509b366378d8328768addccbb73c5f8235c086e
sh-9610 <fkfkthth@naver.com>
2024-01-24 13:57:19 +0900

    refactor : 불필요한 import 삭제

--- src/main/java/sync/slamtalk/map/controller/BasketballCourtController.java
+++ src/main/java/sync/slamtalk/map/controller/BasketballCourtController.java
@@ -2,7 +2,6 @@ package sync.slamtalk.map.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sync.slamtalk.common.ApiResponse;
-import sync.slamtalk.common.BaseException;
 import sync.slamtalk.map.dto.BasketballCourtDto;
 import sync.slamtalk.map.entity.BasketballCourt;
 import sync.slamtalk.map.mapper.BasketballCourtMapper;
