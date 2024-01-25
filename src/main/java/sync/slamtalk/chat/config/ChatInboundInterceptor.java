bca7c02aa102e5fdfec8b06c16cfc097c23bfbc3
yj120 <yeji3852@daum.net>
2024-01-26 03:36:26 +0900

    refactor : import문 최적화

--- src/main/java/sync/slamtalk/chat/config/ChatInboundInterceptor.java
+++ src/main/java/sync/slamtalk/chat/config/ChatInboundInterceptor.java
@@ -9,7 +9,6 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
-import org.springframework.messaging.support.GenericMessage;
 import org.springframework.stereotype.Component;
 import sync.slamtalk.chat.dto.Request.ChatMessageDTO;
 import sync.slamtalk.chat.entity.ChatRoom;
