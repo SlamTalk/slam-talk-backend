68ace4753b4adc8ce954aabaa79328f4ca5209a4
Dong Soo <rsy48@naver.com>
2024-01-25 16:36:24 +0900

    PR 전 커스텀 ErrorResponseCode 수정

--- src/main/java/sync/slamtalk/mate/error/UserErrorResponseCode.java
+++ src/main/java/sync/slamtalk/mate/error/UserErrorResponseCode.java
@@ -10,11 +10,9 @@ public enum UserErrorResponseCode implements ResponseCodeDetails {
     PARTICIPANT_NOT_FOUND(SC_NOT_FOUND, 4042, "해당 참여자를 찾을 수 없습니다."),
     PARTICIPANT_ALREADY_REJECTED(SC_BAD_REQUEST, 4001, "이미 거절된 참여자입니다."),
 
-    PARTICIPANT_ALREADY_ACCEPTED(SC_BAD_REQUEST, 4002, "이미 수락된 참여자입니다."),
     PARTICIPANT_NOT_ALLOWED_TO_CHANGE_STATUS(SC_BAD_REQUEST, 4003, "해당 참여자는 상태를 변경할 수 없습니다."),
 
-    USER_NOT_AUTHORIZED(SC_BAD_REQUEST, 4002, "접근한 유저는 해당 권한이 없습니다."),
-    APPLY_STATUS_NOT_FOUND(SC_NOT_FOUND, 4043, "해당 신청 상태를 찾을 수 없습니다.");
+    USER_NOT_AUTHORIZED(SC_BAD_REQUEST, 4002, "접근한 유저는 해당 권한이 없습니다.");
 
     UserErrorResponseCode(int code, int status, String message) {
         this.code = code;
