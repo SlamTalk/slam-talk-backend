88ab45634d1528bb6d5aadff2b31f24b701e426c
leebongseung <yui5227@gmail.com>
2024-01-26 17:29:54 +0900

    bug : cors 에러 수정

--- src/main/java/sync/slamtalk/security/config/CorsConfig.java
+++ src/main/java/sync/slamtalk/security/config/CorsConfig.java
@@ -6,6 +6,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.Arrays;
+
 @Configuration
 public class  CorsConfig {
    @Bean
@@ -15,10 +17,17 @@ public class  CorsConfig {
       config.setAllowCredentials(true);
       // 해당 경로로 부터 오는 요청들을 허용하겠다고 설정
       config.addAllowedOriginPattern("*");
+      config.addAllowedOrigin("https://slam-talk.vercel.app");
       config.addAllowedOrigin("http://localhost:3000");
       config.addAllowedOrigin("http://localhost:3001");
       config.addAllowedHeader("*");
       config.addAllowedMethod("*");
+
+      // 클라이언트 response header 허용
+      config.setExposedHeaders(Arrays.asList(
+              "Authorization",
+              "Set-Cookie"
+      ));
 
       source.registerCorsConfiguration("/**", config);
       return new CorsFilter(source);
