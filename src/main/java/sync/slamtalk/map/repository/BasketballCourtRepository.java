e9f75c40e4a1dee8d6e1d99970e4538b3291bb95
sh-9610 <fkfkthth@naver.com>
2024-01-26 15:48:35 +0900

    feat : 관리자 상태 검색 메소드 추가

--- src/main/java/sync/slamtalk/map/repository/BasketballCourtRepository.java
+++ src/main/java/sync/slamtalk/map/repository/BasketballCourtRepository.java
@@ -1,11 +1,14 @@
 package sync.slamtalk.map.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import sync.slamtalk.map.entity.AdminStatus;
 import sync.slamtalk.map.entity.BasketballCourt;
 
 @Repository
 public interface BasketballCourtRepository extends JpaRepository<BasketballCourt, Long> {
 
-
+    // AdminStatus에 따른 농구장 목록 조회
+    List<BasketballCourt> findByAdminStatus(AdminStatus adminStatus);
 }
