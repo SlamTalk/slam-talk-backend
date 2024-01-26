ceff7cda9053321013c7cadc60bdeae3a1073bc5
sh-9610 <fkfkthth@naver.com>
2024-01-26 16:01:01 +0900

    feat : 관리자 상태에 따른 특정 농구장 정보 조회 쿼리 메소드 구현

--- src/main/java/sync/slamtalk/map/repository/BasketballCourtRepository.java
+++ src/main/java/sync/slamtalk/map/repository/BasketballCourtRepository.java
@@ -1,6 +1,7 @@
 package sync.slamtalk.map.repository;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sync.slamtalk.map.entity.AdminStatus;
@@ -11,4 +12,8 @@ public interface BasketballCourtRepository extends JpaRepository<BasketballCourt
 
     // AdminStatus에 따른 농구장 목록 조회
     List<BasketballCourt> findByAdminStatus(AdminStatus adminStatus);
+
+    // AdminStatus에 따른 특정 농구장 조회
+    Optional<BasketballCourt> findBycourtIdAndAdminStatus(Long courtId, AdminStatus adminStatus);
+
 }
