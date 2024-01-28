36482cada7e8ebe0b24cfeb0404c0627f59755eb
leebongseung <yui5227@gmail.com>
2024-01-28 13:33:30 +0900

    refactor : 메인 문 수정  - 기존 security 설정 제거

--- src/main/java/sync/slamtalk/SlamtalkApplication.java
+++ src/main/java/sync/slamtalk/SlamtalkApplication.java
@@ -2,20 +2,11 @@ package sync.slamtalk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-
-//@SpringBootApplication
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class SlamtalkApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SlamtalkApplication.class, args);
 	}
 
 }
-
-
-/*
-	Spring Security 의존성을 추가했지만,
-	아직 인증단계를 개발하지 않은 경우 Application에서 exclude를 이용해 Security 기능을 꺼둘 수 있다.
-*/
