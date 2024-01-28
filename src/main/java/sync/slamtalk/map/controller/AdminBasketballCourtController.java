588806681a089638c0a9b71a0139ebd78535902e
sh-9610 <fkfkthth@naver.com>
2024-01-28 15:10:54 +0900

    feat : swagger 기능

--- src/main/java/sync/slamtalk/map/controller/AdminBasketballCourtController.java
+++ src/main/java/sync/slamtalk/map/controller/AdminBasketballCourtController.java
@@ -1,5 +1,6 @@
 package sync.slamtalk.map.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,11 @@ public class AdminBasketballCourtController {
     private final AdminBasketballCourtService adminBasketballCourtService;
 
     @GetMapping("/stand")
+    @Operation(
+            summary = "제보 농구장 정보 관리자 확인", // 기능 제목 입니다
+            description = "이 기능은 제보된 농구장 목록을 확인하는 기능입니다.", // 기능 설명
+            tags = {"지도"}
+    )
     public ApiResponse<List<BasketballCourtDto>> getAllCourtsWithStatusStand() {
         List<BasketballCourtDto> basketballCourtStandDto = adminBasketballCourtService.getAllCourtsWithStatusStand();
         return (ApiResponse.ok(basketballCourtStandDto, "대기중인 농구장 목록을 성공적으로 가져왔습니다."));
