f4641558fa30bb5fe117e0f8af8d8ad75668cacd
leebongseung <yui5227@gmail.com>
2024-01-28 13:40:17 +0900

    refactor : UserRepository에 @Param 어노테이션 추가

--- src/main/java/sync/slamtalk/user/UserRepository.java
+++ src/main/java/sync/slamtalk/user/UserRepository.java
@@ -2,18 +2,23 @@ package sync.slamtalk.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import sync.slamtalk.user.entity.SocialType;
 import sync.slamtalk.user.entity.User;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query("select u from User u where  u.email = :email and u.socialType = :socialType")
-    Optional<User> findByEmailAndSocialType(String email, SocialType socialType);
+    @Query("select u from User u where  u.email = :email and u.socialType = :social_type")
+    Optional<User> findByEmailAndSocialType(
+            @Param("email") String email,
+            @Param("social_type") SocialType socialType
+    );
 
     Optional<User> findByNickname(String nickname);
 
-    @Query("select u from User u where u.refreshToken = :refreshToken")
-    Optional<User> findByRefreshToken(String refreshToken);
+    @Query("select u from User u where u.refreshToken = :refresh_token")
+    Optional<User> findByRefreshToken(@Param("refresh_token") String refreshToken);
 
+    Optional<User> findBySocialTypeAndSocialId(SocialType socialType, String id);
 }
