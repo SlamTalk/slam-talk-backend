3350ef3ff05e70e9d74b8babce647338b25d9dce
leebongseung <yui5227@gmail.com>
2024-01-29 12:10:13 +0900

    feat : 에러 타입 추가   - 마이페이지에 없는 유저 조회하려고 할때 NOT_FOUND_USER 던지는 에러타입 추가

--- src/main/java/sync/slamtalk/user/error/UserErrorResponseCode.java
+++ src/main/java/sync/slamtalk/user/error/UserErrorResponseCode.java
@@ -7,10 +7,11 @@ import static jakarta.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
 
 public enum UserErrorResponseCode implements ResponseCodeDetails {
     INVALID_TOKEN(SC_BAD_REQUEST,4001,"Token Invalid"),
-    LOGIN_FAIL(SC_UNAUTHORIZED,4011,"Failed Login"),
-    EMAIL_ALREADY_EXISTS(SC_BAD_REQUEST, 4012, "이미 존재하는 유저 이메일입니다."),
-    NICKNAME_ALREADY_EXISTS(SC_BAD_REQUEST, 4013, "이미 존재하는 닉네임입니다."),
-    BAD_CREDENTIALS(SC_BAD_REQUEST, 4014, "아이디 또는 비밀번호를 다시 확인해주세요.")
+    LOGIN_FAIL(SC_UNAUTHORIZED,4002,"Failed Login"),
+    EMAIL_ALREADY_EXISTS(SC_BAD_REQUEST, 4003, "이미 존재하는 유저 이메일입니다."),
+    NICKNAME_ALREADY_EXISTS(SC_BAD_REQUEST, 4004, "이미 존재하는 닉네임입니다."),
+    BAD_CREDENTIALS(SC_BAD_REQUEST, 4005, "아이디 또는 비밀번호를 다시 확인해주세요."),
+    NOT_FOUND_USER(SC_BAD_REQUEST, 4006, "해당 유저가 존재하지 않습니다")
     ;
 
     UserErrorResponseCode(int code, int status, String message) {
