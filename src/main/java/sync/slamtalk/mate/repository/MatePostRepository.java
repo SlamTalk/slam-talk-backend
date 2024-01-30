c49eb421abd8970a083633598ccb15b303a8faee
Dong Soo <rsy48@naver.com>
2024-01-30 15:10:42 +0900

    쿼리 수정

--- src/main/java/sync/slamtalk/mate/repository/MatePostRepository.java
+++ src/main/java/sync/slamtalk/mate/repository/MatePostRepository.java
@@ -13,6 +13,5 @@ import java.util.List;
 public interface MatePostRepository extends JpaRepository<MatePost, Long> {
 
     //메이트찾기 목록 조회를 위한 메소드
-    @Query("select m from MatePost m where m.createdAt < ?1 and m.isDeleted = ?2 order by m.createdAt desc")
     List<MatePost> findByCreatedAtLessThanAndIsDeletedNotOrderByCreatedAtDesc(LocalDateTime createdAt, boolean isDeleted, Pageable pageable);
 }
