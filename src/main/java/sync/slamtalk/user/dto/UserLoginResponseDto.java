6b6aca6b9ae5df2dd4c0485763554199d20060ad
leebongseung <yui5227@gmail.com>
2024-01-30 21:00:14 +0900

    refactor : UserLoginResponseDto - 토큰 발급 시 유저정보도 함께 포함시키는 로직으로 변경

--- src/main/java/sync/slamtalk/user/dto/UserLoginResponseDto.java
+++ src/main/java/sync/slamtalk/user/dto/UserLoginResponseDto.java
@@ -2,9 +2,14 @@ package sync.slamtalk.user.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import sync.slamtalk.user.entity.UserRole;
 
 @Getter
 @AllArgsConstructor
 public class UserLoginResponseDto {
+    private Long userId;
+    private String nickname;
+    private String imageUrl;
+    private UserRole userRole;
     private Boolean firstLoginCheck;
 }
