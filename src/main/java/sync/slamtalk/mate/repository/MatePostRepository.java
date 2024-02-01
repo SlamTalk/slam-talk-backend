77144122eb33e480a1b17594a5957ed5603e65ab
leebongseung <yui5227@gmail.com>
2024-02-01 23:42:44 +0900

    feat : 모집상태가 완료된 사용자의 개수를 반환하는 메서드 개발 - 테스트 코드로 정상동작 확인

--- src/main/java/sync/slamtalk/mate/repository/MatePostRepository.java
+++ src/main/java/sync/slamtalk/mate/repository/MatePostRepository.java
@@ -1,5 +1,6 @@
 package sync.slamtalk.mate.repository;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -14,4 +15,13 @@ public interface MatePostRepository extends JpaRepository<MatePost, Long> {
 
     //메이트찾기 목록 조회를 위한 메소드
     List<MatePost> findByCreatedAtLessThanAndIsDeletedNotOrderByCreatedAtDesc(LocalDateTime createdAt, boolean isDeleted, Pageable pageable);
+
+    /* 레벨 시스템 : 모집상태가 완료된 사용자의 개수를 반환하는 메서드 */
+    @Query("select count(*) " +
+            "from MatePost m " +
+            "join Participant p on m.matePostId = p.matePost.matePostId " +
+            "where p.participantId = :userId " +
+            "and m.recruitmentStatus = 'COMPLETED' " +
+            "and p.applyStatus = 'ACCEPTED'")
+    Long findMateCompleteParticipationCount(@Param("userId") Long userId);
 }
