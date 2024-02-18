446d1e6c48384713d7a375040430918578a9c7ad
yj120 <yeji3852@daum.net>
2024-02-18 12:38:19 +0900

    refactor : 테스트코드 일단 삭제

--- src/test/java/sync/slamtalk/chat/redis/RedisServiceTest.java
+++ src/test/java/sync/slamtalk/chat/redis/RedisServiceTest.java
@@ -1,18 +1,21 @@
 package sync.slamtalk.chat.redis;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataRedisTest
 @Import(RedisService.class) // MessageService를 테스트 컨텍스트에 등록
-public class MessageServiceTest {
+@Slf4j
+public class RedisServiceTest {
 
     @Autowired
     private RedisService redisService;
@@ -21,21 +24,7 @@ public class MessageServiceTest {
     private RedisTemplate stringRedisTemplate;
 
     @Test
-    void testSaveMessageWithExpiration() throws InterruptedException {
-        String chatRoomId = "1";
-        String messageId = "testMessageId";
-        String messageContent = "Hello Redis";
-        long timeoutInSeconds = 1; // 1초 후 만료
+    void messageSaveTest(){
 
-        redisService.saveMessage(chatRoomId, messageId, messageContent, timeoutInSeconds);
-
-        // 메시지가 저장되었는지 확인
-        String storedMessage = redisService.getMessage(chatRoomId, messageId);
-        assertThat(storedMessage).isEqualTo(messageContent);
-
-        // 설정한 만료 시간 후에 메시지가 삭제되었는지 확인
-        TimeUnit.SECONDS.sleep(timeoutInSeconds + 1); // 만료 시간보다 조금 더 기다림
-        storedMessage = redisService.getMessage(chatRoomId, messageId);
-        assertThat(storedMessage).isNull();
     }
 }
