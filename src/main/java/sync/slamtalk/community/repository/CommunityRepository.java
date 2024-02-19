8519ec2a139cd8cd92c362e80f7b5930ad8d6d28
leebongseung <yui5227@gmail.com>
2024-02-20 01:45:01 +0900

    feat : 커뮤니티 게시글 개수 조회 하는 메서드 추가

--- src/main/java/sync/slamtalk/community/repository/CommunityRepository.java
+++ src/main/java/sync/slamtalk/community/repository/CommunityRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sync.slamtalk.community.entity.Community;
 import sync.slamtalk.community.entity.CommunityCategory;
+import sync.slamtalk.user.entity.User;
 
 @Repository
 public interface CommunityRepository extends JpaRepository<Community, Long> {
@@ -13,4 +14,7 @@ public interface CommunityRepository extends JpaRepository<Community, Long> {
     List<Community> findByCategoryAndIsDeletedFalse(CommunityCategory category);
     List<Community> findByIsDeletedFalse();
     Optional<Community> findByCommunityIdAndIsDeletedFalse(Long communityId);
+
+    /* 유저가 쓴 커뮤니티의 게시글 개수 조회*/
+    Long countAllByUserAndIsDeletedFalse(User user);
 }
