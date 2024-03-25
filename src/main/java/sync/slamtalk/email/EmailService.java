ca0372ea135f74ae029e8fc18e1647bdbb7ac719
leebongseung <yui5227@gmail.com>
2024-03-25 21:59:42 +0900

    refactor: 타임리프를 이용해서 html을 설정하는것으로 변경

--- src/main/java/sync/slamtalk/email/EmailService.java
+++ src/main/java/sync/slamtalk/email/EmailService.java
@@ -7,6 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 import sync.slamtalk.chat.redis.RedisService;
 import sync.slamtalk.common.BaseException;
 
@@ -18,6 +20,7 @@ import java.util.UUID;
 public class EmailService {
     private final JavaMailSender javaMailSender;
     private final RedisService redisService; //redis 관련
+    private final TemplateEngine templateEngine;
 
     /**
      * 이메일 메세지 생성하는 메서드
@@ -35,36 +38,12 @@ public class EmailService {
             message.addRecipients(Message.RecipientType.TO, email);
             message.setSubject("SlamTalk 인증 번호입니다.", "UTF-8");
 
-            String imageLogoURI = "https://slamtalks3.s3.ap-northeast-2.amazonaws.com/1561891asdfmme1561.png";
-            String text = "<!DOCTYPE html>\n" +
-                    "<html lang=\"ko\">\n" +
-                    "<head>\n" +
-                    "<meta charset=\"UTF-8\" />\n" +
-                    "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
-                    "<title>슬램톡 | 이메일 인증</title>\n" +
-                    "</head>\n" +
-                    "<body>\n" +
-                    "<div class=\"container\" style=\"color: #404040; min-width: 360px; max-width: 600px; margin-top: 50px; padding: 0 300px;\">\n" +
-                    "<div class=\"content\">\n" +
-                    "<img src=\"" + imageLogoURI + "\" alt=\"slam talk logo\" style=\"width: 128px; display: block; margin: 0 auto;\">\n" +
-                    "<h1 style=\"color: black; text-align: center;\">이메일 인증을 진행해주세요</h1>\n" +
-                    "<p style=\"text-align: center; color: black;\">안녕하세요. 슬램톡을 이용해주셔서 감사합니다 :)</p>\n" +
-                    "<p style=\"text-align: center; color: black;\">슬램톡 회원가입을 위해 아래 인증코드를 화면에 입력해주세요.</p>\n" +
-                    "</div>\n" +
-                    "<div class=\"code\" style=\"border-radius: 6px; color: black; height: 120px; margin: 40px 0; background-color: ghostwhite; text-align: center; font-size: 30px;\"><b style=\"line-height: 120px;\">" + code + "</b></div>\n" +
-                    "<p style=\"text-align: center; color: black;\">본 인증코드의 유효기간은 5 분입니다. 시간이 지나면 인증코드 재발급을 해주세요.</p>\n" +
-                    "<div style=\"width: 100%\">\n" +
-                    "<div>\n" +
-                    "<a href=\"https://www.slam-talk.site/signup\" class=\"homepage-btn\" style=\"background-color: #ff634a; color: white; text-align: center; font-size: medium; text-decoration: none; border-radius: 6px; display: block; margin: 30px 0; padding: 10px\">\n" +
-                    "홈페이지로 이동하기\n" +
-                    "</a>\n" +
-                    "</div>\n" +
-                    "</div>\n" +
-                    "</body>\n" +
-                    "</html>";
+            Context context = new Context();
+            context.setVariable("code", code);
+            String text = templateEngine.process("email-template", context);
 
             message.setText(text, "UTF-8", "html");
-            message.setFrom("slamtalk@naver.com"); //보내는사람.
+            message.setFrom("slamtalk@naver.com"); // 보내는사람.
             return message;
         } catch (Exception e){
             log.error("메일 전송 실패 : = {}", e.toString());
