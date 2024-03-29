d8502cc74dcdba5e656a403ae9727a2c7131f2a6
yj120 <yeji3852@daum.net>
2024-03-29 17:13:33 +0900

    refactor : Reflecting feedback

--- src/main/java/sync/slamtalk/chat/dto/request/ChatCreateDTO.java
+++ src/main/java/sync/slamtalk/chat/dto/request/ChatCreateDTO.java
@@ -2,6 +2,7 @@ package sync.slamtalk.chat.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
--- src/main/java/sync/slamtalk/chat/dto/request/ChatMessageDTO.java
+++ src/main/java/sync/slamtalk/chat/dto/request/ChatMessageDTO.java
@@ -3,6 +3,7 @@ package sync.slamtalk.chat.dto.request;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.io.Serializable;
@@ -14,17 +15,17 @@ import java.io.Serializable;
 @Builder(access = AccessLevel.PUBLIC)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ChatMessageDTO implements Serializable {
-    @Nullable
+    @NotNull
     private String messageId; // 메세지 아이디
-    @Nullable
+    @NotNull
     private String roomId; // 채팅방 아이디(채팅방 식별자)
-    @Nullable
+    @NotNull
     private Long senderId; // 메세지를 보낸 사용자의 아이디
-    @Nullable
+    @NotNull
     private String senderNickname; // 메세지를 보낸 사용자의 닉네임 -> 채팅방에 표시될
-    @Nullable
+    @NotNull
     private String imgUrl; // 메세지를 보낸 사용자의 프로필
-    @Nullable
+    @NotNull
     private String content; // 메세지 내용
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private String timestamp; // 메세지를 보낸 시간
