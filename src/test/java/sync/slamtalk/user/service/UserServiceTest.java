67bea2f22fe234ef164f18b6cf2bd14dd49f7c9c
leebongseung <yui5227@gmail.com>
2024-04-04 01:07:24 +0900

    refactor: 테스트코드 1차 작성

--- src/test/java/sync/slamtalk/user/service/UserServiceTest.java
+++ src/test/java/sync/slamtalk/user/service/UserServiceTest.java
@@ -1,23 +1,30 @@
 package sync.slamtalk.user.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import sync.slamtalk.community.repository.CommunityRepository;
+import sync.slamtalk.map.repository.BasketballCourtRepository;
 import sync.slamtalk.mate.repository.MatePostRepository;
+import sync.slamtalk.team.repository.TeamMatchingRepository;
 import sync.slamtalk.user.UserRepository;
-import sync.slamtalk.user.dto.request.UserSignUpReq;
+import sync.slamtalk.user.dto.request.UserUpdateNicknameReq;
+import sync.slamtalk.user.dto.response.UserDetailsMyInfo;
+import sync.slamtalk.user.dto.response.UserDetailsOtherInfo;
 import sync.slamtalk.user.entity.User;
+import sync.slamtalk.user.entity.UserRole;
 import sync.slamtalk.user.repository.UserAttendanceRepository;
 
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @Slf4j
 @ExtendWith(MockitoExtension.class)
@@ -29,37 +36,89 @@ class UserServiceTest {
     private MatePostRepository matePostRepository;
     @Mock
     private UserAttendanceRepository userAttendanceRepository;
+    @Mock
+    private TeamMatchingRepository teamMatchingRepository;
+    @Mock
+    private BasketballCourtRepository basketballCourtRepository;
+    @Mock
+    private CommunityRepository communityRepository;
 
     @InjectMocks
     private UserService userService;
 
-    private Long loginUserId = 1L;
-    private String email = "test@naver.com";
-    private String password = "password";
-    private String nickname = "하이";
+    private User user;
+    private UserUpdateNicknameReq userUpdateNicknameReq;
 
-    @Test
-    @DisplayName("남의 페이지 마이페이지 조회하는 api 테스트")
-    @Disabled
-    void testGenerateNickname() {
-        // given
-        User user = new UserSignUpReq(email, password, nickname).toEntity();
+    @BeforeEach
+    void setUp() {
+        userUpdateNicknameReq = new UserUpdateNicknameReq("새로운 닉네임");
 
-        //when
-        Mockito.when(userRepository.findById(loginUserId))
-                .thenReturn(Optional.of(user));
-        Mockito.when(matePostRepository.findMateCompleteParticipationCount(loginUserId))
-                .thenReturn(1L);
-        Mockito.when(userAttendanceRepository.countUserAttendancesByUser(user))
-                .thenReturn(Optional.of(100L));
-//        UserDetailsMyInfoResponseDto userDetailsMyInfoResponseDto = userService.userDetailsInfo(this.loginUserId, this.loginUserId);
-//
-//        // then
-//        assertThat(userDetailsMyInfoResponseDto.getNickname()).isEqualTo(nickname);
-//        assertThat(userDetailsMyInfoResponseDto.getMateCompleteParticipationCount()).isEqualTo(1L);
-//        assertThat(userDetailsMyInfoResponseDto.getLevel()).isEqualTo(2L);
-//        assertThat(userDetailsMyInfoResponseDto.getLevelScore()).isEqualTo(105L);
+        user = User.builder()
+                .nickname("닉네임")
+                .email("test@test.com")
+                .imageUrl("url")
+                .role(UserRole.USER)
+                .firstLoginCheck(true)
+                .build();
     }
 
+    @Test
+    @DisplayName("[성공] 나의 마이페이지 조회 - 이메일 까지 반환됨")
+    void userDetailsMyInfo() {
+        // given
+        when(userRepository.findById(any())).thenReturn(Optional.of(user));
+        when(teamMatchingRepository.countTeamMatchingByWriter(any())).thenReturn(0L);
+        when(teamMatchingRepository.findTeamMatchingByCompleteParticipationCount(any())).thenReturn(0L);
+        when(basketballCourtRepository.countBasketballCourtByAdminStatusEqualsAndInformerId(any(), any())).thenReturn(0L);
+        when(communityRepository.countAllByUserAndIsDeletedFalse(any())).thenReturn(0L);
 
+        // when
+        UserDetailsMyInfo userDetailsMyInfo = userService.userDetailsMyInfo(any());
+
+        // then
+        assertThat(userDetailsMyInfo.getId()).isEqualTo(user.getId());
+    }
+
+    @Test
+    @DisplayName("[성공] 다른 사용자 마이페이지")
+    void userDetailsOtherInfo() {
+        // given
+        when(userRepository.findById(any())).thenReturn(Optional.of(user));
+        when(teamMatchingRepository.countTeamMatchingByWriter(any())).thenReturn(0L);
+        when(teamMatchingRepository.findTeamMatchingByCompleteParticipationCount(any())).thenReturn(0L);
+        when(basketballCourtRepository.countBasketballCourtByAdminStatusEqualsAndInformerId(any(), any())).thenReturn(0L);
+        when(communityRepository.countAllByUserAndIsDeletedFalse(any())).thenReturn(0L);
+
+        // when
+        UserDetailsOtherInfo userDetailsOtherInfo = userService.userDetailsOtherInfo(any());
+
+        // then
+        assertThat(userDetailsOtherInfo.getId()).isEqualTo(user.getId());
+    }
+
+    @Test
+    @DisplayName("[성공] 자기자신 닉네임 업데이트")
+    void userUpdateNickname() {
+        // when
+        userService.userUpdateNickname(user.getId(), userUpdateNicknameReq);
+
+        // then
+        verify(userRepository).updateUserNickname(user.getId(), userUpdateNicknameReq.getNickname());
+    }
+
+    @Test
+    void userUpdatePositionAndSkillLevel() {
+    }
+
+    @Test
+    void userAttendance() {
+    }
+
+    @Test
+    void updateUserDetailInfo() {
+    }
+
+    @Test
+    void userMyScheduleList() {
+    }
 }
