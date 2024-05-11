6569f5927e10ceede2362ecca2fa0cb923c87d3b
leebongseung <yui5227@gmail.com>
2024-05-11 21:01:17 +0900

    refactor : 파라미터 변수명 수정

--- src/main/java/sync/slamtalk/security/service/CustomUserDetailsService.java
+++ src/main/java/sync/slamtalk/security/service/CustomUserDetailsService.java
@@ -21,11 +21,11 @@ public class CustomUserDetailsService implements UserDetailsService {
     * 유저 이메일과 소셜타입
     * */
    @Override
-   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-      log.debug("유저 정보 = {}", username);
+   public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+      log.debug("유저 정보 = {}", email);
 
        // 해당하는 User 의 데이터가 존재한다면 UserDetails 객체로 만들어서 return
-      return userRepository.findByEmailAndSocialType(username, SocialType.LOCAL)
+      return userRepository.findByEmailAndSocialType(email, SocialType.LOCAL)
               .orElseThrow(() -> new UsernameNotFoundException("해당하는 회원을 찾을 수 없습니다."));
    }
 }
