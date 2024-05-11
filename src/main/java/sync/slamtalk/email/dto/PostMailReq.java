6be1d55719e52a01e9c8ec6c3a199fe9c0ed542e
leebongseung <yui5227@gmail.com>
2024-05-11 16:58:27 +0900

    refactor: dto 이름 컨벤션에 맞게 수정

--- src/main/java/sync/slamtalk/email/dto/PostMailReq.java
+++ src/main/java/sync/slamtalk/email/dto/PostMailReq.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-public class PostMailreq {
+public class PostMailReq {
     @Pattern(
             regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$",
             message = "이메일 형식에 맞지 않습니다."
