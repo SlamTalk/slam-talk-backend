3f54f716cab605bde05a17efeca08adbf31cc57f
sh-9610 <fkfkthth@naver.com>
2024-05-13 00:10:41 +0900

    feature : 농구장 정보 반환 값 수정

--- src/main/java/sync/slamtalk/community/repository/CommentRepository.java
+++ src/main/java/sync/slamtalk/community/repository/CommentRepository.java
@@ -5,11 +5,14 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sync.slamtalk.community.entity.Comment;
+import sync.slamtalk.community.entity.Community;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> findByCommunity_communityIdAndIsDeletedFalse(Long communityId);
     Optional<Comment> findByCommentIdAndIsDeletedFalse(Long commentId);
+
+    Long countByCommunityAndIsDeletedFalse(Community community);
 
 }
